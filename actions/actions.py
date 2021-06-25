f252dfa82ae4561c668a35ee37d334fdd4dda384
DonRuan22 <ruan_donino@hotmail.com>
2021-06-25 08:46:35 -0300

    Add actions

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,87 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
-# class ActionHelloWorld(Action):
+class ActionGreet(Action):
 #
-#     def name(self) -> Text:
-#         return "action_hello_world"
+     def name(self) -> Text:
+         return "greet"
 #
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 #
-#         dispatcher.utter_message(text="Hello World!")
+         dispatcher.utter_message(text="Hey! How are you?")
 #
-#         return []
+         return []
+
+class ActionCheer_up(Action):
+#
+     def name(self) -> Text:
+         return "cheer_up"
+#
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+         dispatcher.utter_message(text="Here is something to cheer you up:")
+#
+         return []
+     
+class ActionHappy(Action):
+#
+     def name(self) -> Text:
+         return "happy"
+#
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+         dispatcher.utter_message(text="Great, carry on!")
+#
+         return []
+     
+class ActionDidHelp(Action):
+#
+     def name(self) -> Text:
+         return "did_that_help"
+#
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+         dispatcher.utter_message(text="Did that help you?")
+#
+         return []
+     
+        
+class ActionBye(Action):
+#
+     def name(self) -> Text:
+         return "action_goodbye"
+#
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+         dispatcher.utter_message(text="Bye!")
+#
+         return []
+
+class ActionBot(Action):
+#
+     def name(self) -> Text:
+         return "action_iamabot"
+#
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+         dispatcher.utter_message(text="I am a bot, powered by Ruan Donino.")
+#
+         return []
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ls
+rasa run actions
 rasa run -p ${PORT} --cors "*"
