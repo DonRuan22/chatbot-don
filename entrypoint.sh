1db16dfffe4816f2feecd2fe3b9f05605e73a35a
DonRuan22 <ruan_donino@hotmail.com>
2021-07-01 13:53:21 -0300

    Credentials to facebook

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ls
 #rasa run actions
-rasa run run --credentials credentials.yml --enable-api --cors “*” --debug
+rasa run --credentials credentials.yml --enable-api --cors “*” --debug
